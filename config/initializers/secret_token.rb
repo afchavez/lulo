84897c39fbe2910c72050da7b7dc30341a1e2c42
Andres chavez <afchavez40@gmail.com>
2013-10-13 13:40:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lulo::Application.config.secret_key_base = 'f9a60ecc18082cdcb924cee64e7a447f80ae516582f20778c448d1fc77203e5d467b32f4d956396e6f57c452e93ffd61c866f1cd5e2a1eed11198b25ab63e6ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lulo::Application.config.session_store :cookie_store, key: '_lulo_session'
