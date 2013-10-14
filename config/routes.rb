b128eae5e506e440beeda6a57c8b5a21c250415d
Andres chavez <afchavez40@gmail.com>
2013-10-13 20:33:35 -0500

    adding bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Lulo::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "new" => "pages#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
